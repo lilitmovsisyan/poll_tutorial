11d8e71a608e2504adb7db2aeceebebbe88ba5fa
Lilit Movsisyan <lilit.momo@gmail.com>
2018-10-03 15:08:30 +0100

    Add __str__ and was_published_recently functions

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,8 +9,18 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200) 
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        #return True is the Question's pub_date is more recent than 1 day ago 
+        # (i.e. greater than or equal to [today - 1 day]). datetime.timedelta() generates a measure of time (e.g. days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
